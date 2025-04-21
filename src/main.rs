d60385b1596781798432f0f509f5b71937b9e25d
nqh2k3 <110041416+VinciHuy@users.noreply.github.com>
2025-04-21 19:39:00 +0700

    add feature

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod p2p;
 
+use core::error;
+use std::{collections::HashSet, thread::spawn, time::Duration};
+
 use chrono::format;
-use libp2p::{core::transport::upgrade, futures::channel::mpsc, identity::ed25519::Keypair, mplex, noise::NoiseConfig, swarm::{self, protocols_handler::multi::Info, SwarmBuilder}, tcp::TokioTcpConfig};
+use libp2p::{core::transport::upgrade, futures::channel::mpsc, identity::ed25519::Keypair, mplex, noise::NoiseConfig, swarm::{self, protocols_handler::multi::Info, SwarmBuilder}, tcp::TokioTcpConfig, Swarm};
 use log::{info, warn};
+use p2p::BLOCK_TOPIC;
+use serde_json::json;
 use sha2::Sha256;
+use tokio::time::sleep;
 
 
 pub struct App {
@@ -194,4 +200,121 @@ async  fn main() {
             spawn(fut);
         }))
         .build();
+
+    let mut stdin = BufReader::new(stdin()).lines();
+
+    Swarm::listen_on(&mut swarm, "/ip/0.0.0.0/tcp/0"
+        .parse()
+        .expect("Can get a local socket"),
+    )
+    .expect("Swarm can be started");
+    
+    spawn(async move{
+        sleep(Duration::from_secs(1)).await;
+        info!("Sending init event");
+        init_sender.sen(true).expect("Can send init event");
+    });
+
+
+    loop{
+        let evt ={
+            select! {
+                line = stdin.next_line() => Some(p2p::EventType::Input(line.expect("Can get line").expect("Can read line from stdin"))),
+                respone = response_rcv.recv() =>{
+                    Some(p2p::EventType::LocalChainRespone(respone.expect("Respone exists")))
+                },
+                _init = init_rcv.recv() =>{
+                    Some(p2p::EventType::Init)
+                }
+
+                event = swarm.select_next_some() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+            }
+
+        };
+
+        if let Some(event) = evt{
+            match event{
+                p2p::EvenType::Init =>{
+                    let peers = p2p::get_lisr_peers(&Swarm);
+                    swarm.behaviour_mut().app.genesis();
+                    
+                    info!("Connect nodes: {}", peers.len());
+                    if !peers.is_empty(){
+                        let req = p2p::LocalChainRequest{
+                            from_peer_id: peers
+                            .iter()
+                            .last()
+                            .expect("At last one peer")
+                            .to_string(),
+                        };
+
+                        let json = serde_json::to_string(&req).expect("Can jsonfy request");
+                        swarm
+                        .behaviour_mut()
+                        .floodsub
+                        .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+
+                        
+                    }
+                }
+                p2p::EvenType::LocalChainResponse(resp) => {
+                    let json = serde_json::to_string(&resp).expect("Can jsonfy respone");
+                    swarm
+                    .behaviour_mut()
+                    .floodsub
+                    .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                }
+                p2p::EvenType::Input(line) => match  line.as_str() {
+                    "ls p" => p2p::handle_print_peers(&swarm),
+                    cmd if cmd.starts_with("ls c") => p2p::handle_print+chain(&swarm),
+                    cmd if cmd.starts_with("create b") => p2p::handle_create_block(cmd, &mut swarm),
+                    _ => error("Unknown command"),
+                    
+                },
+            }
+        }
+    }
+
+    pub fn get_lisr_peers(swarm: &Swarm) -> Vec {
+        info!("Discover");
+        let nodes = swarm.behaviour().mdns.discovered_nodes();
+        let mut unique_peers = HashSet::new();
+        for peer in nodes {
+            unique_peers.insert(peer);
+        }
+        unique_peers.iter().map(|p| p.to_string().collect())
+    }
+
+    pub fn handle_print_peers(swarm: &Swarm){
+        let peers = get_lisr_peers(swarm);
+        peers.iter().for_each(|p| info!("{}", p));
+    }
+
+    pub fn handle_create_block(cmd: &str, swarm: &mut Swarm){
+        if let Some(data) = cmd.strip_prefix("create b"){
+            let behaviour = swarm.behaviour_mut();
+            let latest_block = behaviour
+                            .app
+                            .blocks
+                            .last()
+                            .expect("There is last one block");
+
+            let block = Block::new(
+                latest_block.id + 1,
+                latest_block.hash.clone(),
+                data.to_owned(),
+            );
+
+            let json = serde_json::to_string(&block).expect("Can jsonfy request");
+            behaviour.app.blocks.push(block);
+            info!("Broadcasting new block");
+            behaviour
+                .floodsub
+                .publish(BLOCK_TOPIC.clone(), json.as_bytes());            
+        }
+    }
+
 }
